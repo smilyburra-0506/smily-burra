0bd192e46b9cd84204a6aeca4b6c563109f7a31a
smilyburra-0506 <smilyburra@gmail.com>
2026-03-13 12:09:14 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const StudentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    course: String
+});
+module.exports = mongoose.model("Student",
+    StudentSchema);
