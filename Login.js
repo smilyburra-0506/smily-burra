950c483c399e956cf206e19e207bd2e1c35c7466
smilyburra-0506 <smilyburra@gmail.com>
2026-04-17 10:29:51 +0530

    Add files via upload

--- Login.js
+++ Login.js
@@ -1,55 +1,48 @@
 import { useState, useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AutContext, AuthContext } from "../context/AuthContext";
 
 function Login() {
-  const [form, setForm] = useState({
-    username: "",
-    password: ""
-  });
-
-  const { login } = useContext(AuthContext);
-
-  const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value
+    const [form, setForm] = useState({
+        username: "",
+        password: ""
     });
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const { login } = useContext(AuthContext);
 
-    const stored = JSON.parse(localStorage.getItem("userData"));
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
 
-    if (
-      stored.username === form.username &&
-      stored.password === form.password
-    ) {
-      login(form);
-    } else {
-      alert("Invalid Credentials");
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const stored = JSON.parse(localStorage.getItem("userData"));
+        if(
+            stored.username === form.username &&
+            stored.username === form.password
+        ) {
+            login(form);
+        }  else {
+            alert("Inavalid Creadentials");
+        }
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+
+            <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            />
+            <button type="submit">Login</button>
+            </form>
+    );
+
+
+
     }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
-
-      <input
-        type="text"
-        name="username"
-        onChange={handleChange}
-      />
-
-      <input
-        type="password"
-        name="password"
-        onChange={handleChange}
-      />
-
-      <button type="submit">Login</button>
-    </form>
-  );
-}
-
-export default Login;
+    export default Login
